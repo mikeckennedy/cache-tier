31ae39ca6a5e7d4432a9ac4b4cda963e8f85bff9
Michael Kennedy <mikeckennedy@gmail.com>
2015-08-10 13:47:05 -0700

    setup.

--- src/client/cache-tier-client-pkg/setup.py
+++ src/client/cache-tier-client-pkg/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='cache_tier',
     packages=['cache_tier'],
-    version='1.0.2',
+    version='1.0.3',
     description='Imagine you have a set of static files you want to serve to '
                 'the world. Cache-tier allows you to quickly spin up a Linux '
                 'web server in a location with cheap, plentiful bandwidth and '
@@ -11,7 +11,7 @@ setup(
     author='Michael Kennedy - @mkennedy',
     author_email='mikeckennedy+pypi@gmail.com',
     url='https://github.com/mikeckennedy/cache-tier',
-    download_url='https://github.com/mikeckennedy/cache-tier/tarball/1.0.2',
+    download_url='https://github.com/mikeckennedy/cache-tier/tarball/1.0.3',
     license='MIT License',
     keywords=['caching', 'cache-tier', 'cache', 'tier'],
     classifiers=[],
