9bfb58b74a4b879834b6fc995a4c408cae294a15
Michael Kennedy <mikeckennedy@gmail.com>
2015-08-10 12:19:46 -0700

    cleanup.

--- src/client/cache-tier-client-pkg/tests/test.py
+++ src/client/cache-tier-client-pkg/tests/test.py
@@ -3,12 +3,16 @@ import cache_tier
 
 def main():
     base_url = "http://localhost:5000/"
-    # base_url = "http://downloads.talkpythontome.com/"
     file = '016-Netflix-roy-rapaport.mp3'
 
     while True:
         input("Enter to simulate request")
-        cache = cache_tier.CacheTierClient(base_url, local_cache_time=20, log_enabled=True)
+
+        cache = cache_tier.CacheTierClient(
+            base_url,
+            local_cache_time=20,
+            log_enabled=True)
+
         if cache.verify_file(file):
             print("File validated, would download at:")
             print(cache.build_download_url(file))
